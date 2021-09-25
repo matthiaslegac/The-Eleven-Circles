9bb2fb88cf01b774f48f47afa1623ac76793e988
U203128R <65113608+matthiaslegac@users.noreply.github.com>
2021-09-25 16:09:53 +0200

    Réglage du beug de saut

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,8 +10,9 @@ public class PlayerMovement : MonoBehaviour
     private bool isJumping;
     private bool isGrounded;
 
-    public Transform groundCheckLeft;
-    public Transform groundCheckRight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask collisionLayers;
 
     public Rigidbody2D rb;
     public Animator animator;
@@ -19,25 +20,30 @@ public class PlayerMovement : MonoBehaviour
     
 
     private Vector3 velocity = Vector3.zero;
+    private float horizontalMouvement;
 
-
-    void FixedUpdate()
+    void Update()
     {
-        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
-
-        float horizontalMouvement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+       
+        horizontalMouvement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
 
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
         }
-        MovePlayer(horizontalMouvement);
 
         Flip(rb.velocity.x);
 
         float characterVelocity = Mathf.Abs(rb.velocity.x);
         animator.SetFloat("Speed", characterVelocity);
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
+
+        MovePlayer(horizontalMouvement);
     }
 
     void MovePlayer(float _horizontalMouvement)
@@ -61,5 +67,11 @@ public class PlayerMovement : MonoBehaviour
         {
             spriteRenderer.flipX = true;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
